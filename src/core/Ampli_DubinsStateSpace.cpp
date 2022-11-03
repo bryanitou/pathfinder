1a4667b4c64d5b11ad1ad1ca94d9bccdf5d15d6b
Bryan Cabrera <bryangcr25@gmail.com>
2022-11-03 10:18:16 +0100

    Create new Amplification class of Dubins.

---
+++ src/core/Ampli_DubinsStateSpace.cpp
@@ -0,0 +1,6 @@
+/**
+ * Amplification inheritance of the main DubinsClass
+ * - Enhancement of the DubinsStateSpace Class.
+ */
+
+# include "Ampli_DubinsStateSpace.h"
---
+++ src/core/Ampli_DubinsStateSpace.h
@@ -0,0 +1,40 @@
+/**
+ * Amplification inheritance of the main DubinsClass
+ * - Enhancement of the DubinsStateSpace Class.
+ */
+
+# include "ompl/base/spaces/DubinsStateSpace.h"
+
+class Ampli_DubinsStateStateSpace : public ompl::base::DubinsStateSpace
+{
+public:
+
+    /**
+     * Default conistructor
+     * @param turningRadius
+     * @param isSymmetric
+     */
+    explicit Ampli_DubinsStateStateSpace(double turningRadius = 1.0, bool isSymmetric = false)
+            : ompl::base::DubinsStateSpace(turningRadius, isSymmetric)
+    {
+
+    }
+
+    /**
+     * Default destructor
+     */
+    ~ Ampli_DubinsStateStateSpace() override = default;
+
+
+public:
+
+    /**
+     * Gets the current set radius in the DubinsStateSpace.
+     * @return double
+     */
+    double getRadius()
+    {
+        return this->rho_;
+    }
+
+};
