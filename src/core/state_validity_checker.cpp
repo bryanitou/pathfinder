bd25c1ffcb13ffc052ebf2259f4535b5739fea1d
Bryan Cabrera <bryangcr25@gmail.com>
2022-11-12 19:36:14 +0100

    Document state validity functions.

--- src/core/state_validity_checker.cpp
+++ src/core/state_validity_checker.cpp
@@ -1,19 +1,25 @@
-//
-// Created by bryan on 12/11/22.
-//
+/**
+ * State validity functions
+ */
 
 #include "state_validity_checker.h"
 
 bool state_validity_checker::isStateValidSquares(const ompl::base::SpaceInformation *si, const ompl::base::State *state,
                                                  const std::vector<simple_square*>* squares)
 {
+    // Set the State Space
     const auto *s = state->as<ompl::base::SE2StateSpace::StateType>();
+
+    // Get the coordinates
     double x=s->getX(), y=s->getY();
 
-    // Iterate through all the squares
+    // Iterate through all the squares and check bounds
     for (const auto & sq : *squares)
     {
+        // Check if it is inside
         bool is_in_a_square = sq->is_in_square(x, y);
+
+        // Assess bool
         if (is_in_a_square)
         {
             return false;
